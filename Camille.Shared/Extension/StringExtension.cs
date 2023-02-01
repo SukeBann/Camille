2c67fdf77eae0a211dd986e7627eb834dbd810b1
yangzhenyu <yangzhenyu@lekevr.com>
2023-02-01 18:45:04 +0800

    添加编程工具共享项目， 添加空的字符串扩展方法文件

--- Camille.Shared/Extension/StringExtension.cs
+++ Camille.Shared/Extension/StringExtension.cs
@@ -1,15 +1,9 @@
-﻿namespace Camille.Client.Extension;
+﻿using Newtonsoft.Json.Linq;
+using SixLabors.Fonts.Tables.TrueType;
+
+namespace Camille.Client.Extension;
 
 public static class StringExtension
 {
-    /// <summary>
-    /// 获取json文本中的
-    /// </summary>
-    /// <param name="jsonString"></param>
-    /// <param name="nodeName"></param>
-    /// <returns></returns>
-    public static string GetJsonNode(this string jsonString, string nodeName)
-    {
-        
-    }
+
 }
