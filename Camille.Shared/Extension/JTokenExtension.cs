ab278108b558c4e127354a9cd913f1876076c49a
SukeBan <sukeban@qq.com>
2023-02-07 18:18:59 +0800

    添加一些string的扩展方法(json方面)， 添加了JToken相关的扩展方法

---
+++ Camille.Shared/Extension/JTokenExtension.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Camille.Client.Extension;
+
+/// <summary>
+/// Json-Object-Array-Token的扩展
+/// </summary>
+public static class JTokenExtension
+{
+   /// <summary>
+   /// 尝试将JToken 转换为 JObject
+   /// </summary>
+   /// <param name="jToken"></param>
+   /// <param name="jObject"></param>
+   /// <returns></returns>
+   public static bool TryGetJObject(this JToken jToken, out JObject? jObject)
+   {
+      jObject = null;
+      if (jToken is not JObject jsonObject) return false;
+      
+      jObject = jsonObject; 
+      return true;
+   }
+}
--- Camille.Shared/Extension/StringExtension.cs
+++ Camille.Shared/Extension/StringExtension.cs
@@ -1,9 +1,38 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using SixLabors.Fonts.Tables.TrueType;
 
 namespace Camille.Client.Extension;
 
 public static class StringExtension
 {
+    /// <summary>
+    /// 如果json文本可以解析为JObject则返回true, 否则返回false
+    /// </summary>
+    /// <param name="jsonText">要解析的json文本</param>
+    /// <param name="jToken">如果能解析则返回JToken, 否则返回null</param>
+    /// <returns></returns>
+    public static bool TryGetJObject(this string jsonText, out JToken? jToken)
+    {
+        jToken = null;
+        try
+        {
+            jToken = JToken.Parse(jsonText);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
 
+    /// <summary>
+    /// 判断是否为正确的json文本
+    /// </summary>
+    /// <param name="jsonText">要解析的json文本</param>
+    /// <returns></returns>
+    public static bool IsValidJson(this string jsonText)
+    {
+        return TryGetJObject(jsonText, out _);
+    }
 }
