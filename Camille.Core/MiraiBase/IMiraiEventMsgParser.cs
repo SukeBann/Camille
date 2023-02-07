c5c944fc2935cbf1a97aa8adb8a2d851efd49898
SukeBan <sukeban@qq.com>
2023-02-07 18:21:32 +0800

    修改数据解析器接口的定义, 添加数据解析器的实现

--- Camille.Core/MiraiBase/IMiraiEventMsgParser.cs
+++ Camille.Core/MiraiBase/IMiraiEventMsgParser.cs
@@ -1,9 +1,28 @@
-﻿namespace Camille.Core.MiraiBase;
+﻿using System.Reactive.Subjects;
+using Camille.Core.Adapter;
+
+namespace Camille.Core.MiraiBase;
 
 /// <summary>
-/// 将Websocket收到的文本数据解析为事件或消息<see cref=""/>类型
+/// 将Websocket收到的文本数据解析为事件<see cref="IMiraiEvent"/>或消息<see cref="IMiraiMessageReceived"/>类型
+/// 并通过Rx通知接受到的信息或事件
 /// </summary>
 public interface IMiraiEventMsgParser
 {
+    /// <summary>
+    /// 收到新的Mirai事件
+    /// </summary>
+    public Subject<IMiraiEvent> OnMiraiEventReceived { get; set; }
     
+    /// <summary>
+    /// 收到新的Mirai信息
+    /// </summary>
+    public Subject<IMiraiMessageReceived> OnMiraiMessageReceived { get; set; }
+
+    /// <summary>
+    /// 开始解析收到的数据, 需要在这个方法里面实现订阅消息接收器发出的数据,
+    /// <br/> 并且设置解析数据的方法, 并通过<see cref="OnMiraiEventReceived"/>和<see cref="OnMiraiMessageReceived"/>将数据传播出去
+    /// </summary>
+    /// <param name="dataSender">数据发送者</param>
+    public void BeginParseData(IReceiveDataPublisher dataSender);
 }
---
+++ Camille.Imp/MiraiBase/MiraiEventMsgParser.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+using System.Reactive.Subjects;
+using Camille.Client.Extension;
+using Camille.Core.Adapter;
+using Camille.Core.MiraiBase;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Camille.Imp.MiraiBase;
+
+public class MiraiEventMsgParser : IMiraiEventMsgParser
+{
+    public void BeginParseData(IReceiveDataPublisher dataSender)
+    {
+        dataSender.OnWsReceiveMsg.Subscribe(ParseData);
+    }
+
+    #region Method
+
+    private void ParseData(string data)
+    {
+        if (!data.TryGetJObject(out var jToken)) return;
+
+        if (!jToken.TryGetJObject(out var jObject)) return;
+
+        if (jObject.TryGetValue("type", out var value))
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// 尝试从type获取对应的mirai事件
+    /// </summary>
+    /// <param name="type">类型</param>
+    /// <param name="json">接受到的json数据</param>
+    /// <param name="miraiEvent">成功获取事件时out event, 没有获取到时out null</param>
+    /// <returns>成功获取返回true, 获取失败返回false</returns>
+    private bool TryGetMiraiEvent(string type, string json, out IMiraiEvent? miraiEvent)
+    {
+    }
+
+    /// <summary>
+    /// 尝试从type获取对应的mirai信息
+    /// </summary>
+    /// <param name="type">类型</param>
+    /// <param name="json">接受到的json数据</param>
+    /// <param name="miraiMessageReceived">成功获取事件时out message, 没有获取到时out null</param>
+    /// <returns>成功获取返回true, 获取失败返回false</returns>
+    private bool TryGetMiraiMessage(string type, string json, out IMiraiMessageReceived? miraiMessageReceived)
+    {
+        
+    }
+
+    #endregion
+
+    #region Publish
+
+    /// <inheritdoc/>
+    public Subject<IMiraiEvent> OnMiraiEventReceived { get; set; }
+
+    /// <inheritdoc/>
+    public Subject<IMiraiMessageReceived> OnMiraiMessageReceived { get; set; }
+
+    #endregion
+}
